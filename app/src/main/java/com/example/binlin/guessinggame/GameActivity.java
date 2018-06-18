7c03c4095e0895bddd81e8d9fbecec1be7ef41ed
Bin Lin <binlin2000@gmail.com>
2018-06-18 12:16:26 -0400

    finished project

--- app/src/main/java/com/example/binlin/guessinggame/GameActivity.java
+++ app/src/main/java/com/example/binlin/guessinggame/GameActivity.java
@@ -3,7 +3,6 @@ package com.example.binlin.guessinggame;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +19,7 @@ public class GameActivity extends AppCompatActivity {
     private final int MAX_GUESS_COUNT = 4;
     public static final String winningNumber = "WINNING_NUMBER";
 
+    // TODO to reset the game
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,11 +29,19 @@ public class GameActivity extends AppCompatActivity {
         clue = findViewById(R.id.clue_textView);
         guess = findViewById(R.id.guess_editText);
 
-        // this generate a random number between 1 and 100
-        generatedNumber = (int) Math.ceil(Math.random() * 100);
         // Toast.makeText(this, Integer.toString(generatedNumber), Toast.LENGTH_SHORT).show();
 
         setListeners();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // this generate a random number between 1 and 100
+        generatedNumber = (int) Math.ceil(Math.random() * 100);
+        numberOfGuesses = 0;
+        clue.setVisibility(View.INVISIBLE);
+        guess.setText("");
     }
 
     private void setListeners() {
@@ -50,14 +58,14 @@ public class GameActivity extends AppCompatActivity {
         try {
             int userGuess = Integer.parseInt(guess.getText().toString());
             if (userGuess > 100 || userGuess <= 0) {
-                clue.setText("Enter a Number Between 1 and 100");
+                clue.setText(R.string.between_number);
                 clue.setVisibility(View.VISIBLE);
                 guess.setText("");
             } else {
                 checkGuess(userGuess);
             }
         } catch (NumberFormatException exception) {
-            clue.setText("Please Enter a Number!");
+            clue.setText(R.string.enter_number);
             clue.setVisibility(View.VISIBLE);
         }
 
@@ -82,14 +90,16 @@ public class GameActivity extends AppCompatActivity {
             clue.setVisibility(View.VISIBLE);
             guess.setText("");
             numberOfGuesses++;
+            Toast.makeText(this, getString(R.string.chances_left, (5 - numberOfGuesses)),Toast.LENGTH_SHORT).show();
 
 
         } else if (userGuess > generatedNumber) {
             // Update clue textView to say lower, make sure the visibility is visible, set guess EditText to "" and increment numberOfGuess by 1
-            clue.setText("lower");
+            clue.setText(R.string.lower);
             clue.setVisibility(View.VISIBLE);
             guess.setText("");
             numberOfGuesses++;
+            Toast.makeText(this, getString(R.string.chances_left, (5 - numberOfGuesses)),Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/com/example/binlin/guessinggame/ResultsActivity.java
+++ app/src/main/java/com/example/binlin/guessinggame/ResultsActivity.java
@@ -1,15 +1,56 @@
 package com.example.binlin.guessinggame;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static com.example.binlin.guessinggame.GameActivity.winningNumber;
 
 public class ResultsActivity extends AppCompatActivity {
+
+    private Button playAgainButton;
+    private TextView correctNumberTextView;
+    private TextView resultTextView;
+    private ImageView resultImageView;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        playAgainButton = findViewById(R.id.restart_button);
+        resultTextView = findViewById(R.id.results_textView);
+        correctNumberTextView = findViewById(R.id.number_textView);
+        resultImageView = findViewById(R.id.winning_imageView);
 
-        // TODO get winning number from intent if user has lost
+        setListeners();
+        
+        intent = getIntent();
+        if(intent.hasExtra(winningNumber)){
+            setLosingData();
+        }
+    }
+
+    private void setLosingData() {
+        int winningNumber = intent.getIntExtra(GameActivity.winningNumber, 0);
+        resultTextView.setText(R.string.you_lost);
+        correctNumberTextView.setText(getString(R.string.winning_number, winningNumber));
+        correctNumberTextView.setVisibility(View.VISIBLE);
+
+        resultImageView.setImageResource(R.drawable.losingsadface);
+    }
+
+    private void setListeners() {
+        playAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
